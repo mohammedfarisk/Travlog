d3fd0dc7f4dd45dd2ef02c27ab81146db722fdea
Mohammed Faris K <mohammedfaris858@gmail.com>
2019-08-14 23:18:09 +0530

    firestore gradle codes added

--- app/src/main/java/com/example/travlog/AddNewLocation.java
+++ app/src/main/java/com/example/travlog/AddNewLocation.java
@@ -2,19 +2,21 @@ package com.example.travlog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.AdapterView;
 
-import com.google.firebase.database.core.view.View;
-
 import java.lang.String;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AddNewLocation extends AppCompatActivity{
 
-    Spinner continentSpinner, countrySpinner;
+    Spinner continentSpinner, countrySpinner, statesSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,8 @@ public class AddNewLocation extends AppCompatActivity{
         //continents spinner
         continentSpinner = (Spinner) findViewById(R.id.continentsSpinner);
         countrySpinner = (Spinner) findViewById(R.id.countriesSpinner);
+        statesSpinner = (Spinner) findViewById(R.id.spinnerStates);
+
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.continents_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -34,39 +38,49 @@ public class AddNewLocation extends AppCompatActivity{
         continentSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
             @Override
             public void onItemSelected(AdapterView<?> adapterView, android.view.View view, int i, long l) {
-                String text = continentSpinner.getSelectedItem().toString();
-                switch (text){
-                    case "Africa":
-                        ArrayAdapter<CharSequence> adapter_country_africa = ArrayAdapter.createFromResource(AddNewLocation.this.getApplicationContext(), R.array.countries_africa_array, android.R.layout.simple_spinner_item);
-                        adapter_country_africa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                        countrySpinner.setAdapter(adapter_country_africa);
-                        break;
-                    case "Asia":
-                        ArrayAdapter<CharSequence> adapter_country_asia = ArrayAdapter.createFromResource(AddNewLocation.this.getApplicationContext(), R.array.countries_asia_array, android.R.layout.simple_spinner_item);
-                        adapter_country_asia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                        countrySpinner.setAdapter(adapter_country_asia);
-                        break;
-                    case "Europe":
-                        ArrayAdapter<CharSequence> adapter_country_europe = ArrayAdapter.createFromResource(AddNewLocation.this.getApplicationContext(), R.array.countries_europe_array, android.R.layout.simple_spinner_item);
-                        adapter_country_europe.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                        countrySpinner.setAdapter(adapter_country_europe);
-                        break;
-                    case "North America":
-                        ArrayAdapter<CharSequence> adapter_country_north_america = ArrayAdapter.createFromResource(AddNewLocation.this.getApplicationContext(), R.array.countries_north_america_array, android.R.layout.simple_spinner_item);
-                        adapter_country_north_america.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                        countrySpinner.setAdapter(adapter_country_north_america);
-                        break;
-                    case "Australia":
-                        ArrayAdapter<CharSequence> adapter_country_australia = ArrayAdapter.createFromResource(AddNewLocation.this.getApplicationContext(), R.array.countries_australia_array, android.R.layout.simple_spinner_item);
-                        adapter_country_australia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                        countrySpinner.setAdapter(adapter_country_australia);
-                        break;
-                    case "South America":
-                        ArrayAdapter<CharSequence> adapter_country_south_america = ArrayAdapter.createFromResource(AddNewLocation.this.getApplicationContext(), R.array.countries_south_america_array, android.R.layout.simple_spinner_item);
-                        adapter_country_south_america.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                        countrySpinner.setAdapter(adapter_country_south_america);
-                        break;
-                }
+
+                //getting name of selected continent and removing whitespace and changing complete name to lowercase
+                String continent = continentSpinner.getSelectedItem().toString();
+
+                String text = continent.replaceAll("\\s+", "");
+                text = text.toLowerCase();
+
+                //creating name of corresponding countrie's xml string array
+                String continentName = "countries_"+text+"_array";
+
+                //fetching the list of countries of selected continent and storing it in to an array
+                int countriesArrayID= getResources().getIdentifier(continentName , "array",AddNewLocation.this.getPackageName());
+                String[] items = getResources().getStringArray(countriesArrayID);
+
+                //showing list of countries as spinner dropdown items
+                ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(AddNewLocation.this.getApplicationContext(),   android.R.layout.simple_spinner_item, items);
+                spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+                countrySpinner.setAdapter(spinnerArrayAdapter);
+
+                countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                        String country = countrySpinner.getSelectedItem().toString();
+
+                        country = country.replaceAll("\\s+", "");
+                        country = country.toLowerCase();
+                        String stateName = "states_"+country+"_array";
+
+                        int statesArrayID= getResources().getIdentifier(stateName , "array",AddNewLocation.this.getPackageName());
+                        String[] items = getResources().getStringArray(statesArrayID);
+                        System.out.println("STATE = "+items[0]);
+
+                        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(AddNewLocation.this.getApplicationContext(),   android.R.layout.simple_spinner_item, items);
+                        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+                        statesSpinner.setAdapter(spinnerArrayAdapter);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
             }
 
             @Override
